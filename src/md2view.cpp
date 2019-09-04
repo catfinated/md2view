5db011b0b699bdea61ea6a8a7cbead09b292355a
catfinated <20151825+catfinated@users.noreply.github.com>
2019-09-03 22:28:42 -0400

    update camera interface

--- src/md2view.cpp
+++ src/md2view.cpp
@@ -1,7 +1,6 @@
 #include "engine.hpp"
 #include "camera.hpp"
 #include "md2.hpp"
-#include "quad.hpp"
 #include "model_selector.hpp"
 
 #include "glm/gtx/string_cast.hpp"
@@ -35,10 +34,8 @@ private:
 private:
     std::shared_ptr<blue::Shader> shader_;
     Camera camera_;
-    std::unique_ptr<TexturedQuad> quad_;
     boost::program_options::options_description options_;
     std::string models_dir_;
-    std::string anim_name_;
     ModelSelector ms_;
     bool vsync_enabled_ = false;
     float scale_ = 64.0f;
@@ -74,13 +71,11 @@ void MD2View::reset_model_matrix()
     scale_ = 64.0f;
 
     pos_ = glm::vec3(0.0f, 0.0f, 0.0f);
-
 }
 
 void MD2View::reset_camera()
 {
-    camera_.reset();
-    camera_.Position = glm::vec3(0.0f, 0.0f, 3.0f);
+    camera_.reset(glm::vec3(0.0f, 0.0f, 3.0f));
 }
 
 void MD2View::load_current_texture(EngineBase& engine)
@@ -102,13 +97,11 @@ bool MD2View::on_engine_initialized(EngineBase& engine)
     glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
 
     shader_ = engine.resource_manager().load_shader("md2.vert", "md2.frag", nullptr, "md2");
-    //engine.resource_manager().load_shader("md2_model.vert", "md2_model.frag", nullptr, "md2_model");
-    engine.resource_manager().load_shader("blending.vert", "blending.frag", nullptr, "quad");
     std::cout << ms_.model_name() << '\n';
     std::cout << ms_.model().skins().size() << '\n';
     load_current_texture(engine);
 
-    camera_.Position = glm::vec3(0.0f, 0.0f, 3.0f);
+    camera_.set_position(glm::vec3(0.0f, 0.0f, 3.0f));
 
     glEnable(GL_DEPTH_TEST);
     glEnable(GL_CULL_FACE);
@@ -121,8 +114,7 @@ bool MD2View::on_engine_initialized(EngineBase& engine)
 
 void MD2View::on_framebuffer_resized(float aspect_ratio)
 {
-    //auto& shader = engine.resource_manager().shader("md2");
-   glm::mat4 projection = glm::perspective(glm::radians(camera_.Zoom),
+    glm::mat4 projection = glm::perspective(glm::radians(camera_.zoom()),
                                            aspect_ratio,
                                            0.1f, 500.0f);
 
@@ -135,14 +127,12 @@ void MD2View::render(EngineBase& engine)
 
     //ImGui::ShowTestWindow(nullptr);
     //return;
-    //std::cout << " model path=" << ms_.model_path() << '\n';
 
-    //auto& shader = engine.resource_manager().shader("md2");
     auto& texture = engine.resource_manager().texture2D(ms_.model().current_skin().fpath);
     //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
-    glm::mat4 view = camera_.GetViewMatrix();
-    glm::mat4 projection = glm::perspective(glm::radians(camera_.Zoom),
+    glm::mat4 view = camera_.view_matrix();
+    glm::mat4 projection = glm::perspective(glm::radians(camera_.zoom()),
                                             engine.aspect_ratio(),
                                             0.1f, 500.0f);
 
@@ -152,7 +142,6 @@ void MD2View::render(EngineBase& engine)
     glm::quat rotx = glm::angleAxis(rot_[0], glm::vec3(1.0f, 0.0f, 0.0f));
     glm::quat roty = glm::angleAxis(rot_[1], glm::vec3(0.0f, 1.0f, 0.0f));
     glm::quat rotz = glm::angleAxis(rot_[2], glm::vec3(0.0f, 0.0f, 1.0f));
-    //model = glm::rotate(model, glm::radians(-90.0f), glm::vec3(0.0f, 1.0f, 0.0f));
     model *= glm::mat4_cast(roty * rotz * rotx);
     auto s = 1.0f / scale_; // uniform scale factor
     model = glm::scale(model, glm::vec3(s, s, s));
@@ -165,16 +154,12 @@ void MD2View::render(EngineBase& engine)
     texture.bind();
     ms_.model().draw(*shader_);
     glCheckError();
-    //quad_->Draw(shader);
 
     // draw gui
     ImGui::Begin("Model");
     ImGui::Text("Camera");
 
-    ImGui::InputFloat3("Position", glm::value_ptr(camera_.Position), -1, ImGuiInputTextFlags_ReadOnly);
-    ImGui::InputFloat3("Front", glm::value_ptr(camera_.Front), -1, ImGuiInputTextFlags_ReadOnly);
-    ImGui::InputFloat3("Up", glm::value_ptr(camera_.Up), -1, ImGuiInputTextFlags_ReadOnly);
-    ImGui::InputFloat3("Right", glm::value_ptr(camera_.Right), -1, ImGuiInputTextFlags_ReadOnly);
+    camera_.draw_ui();
 
     if (ImGui::Button("Reset Camera")) {
         reset_camera();
@@ -252,22 +237,22 @@ void MD2View::update(EngineBase& engine, GLfloat delta_time)
 void MD2View::process_input(EngineBase& engine, GLfloat delta_time)
 {
     if (engine.keys()[GLFW_KEY_W]) {
-        camera_.ProcessKeyboard(FORWARD, delta_time);
+        camera_.move(Camera::FORWARD, delta_time);
     }
     if (engine.keys()[GLFW_KEY_S]) {
-        camera_.ProcessKeyboard(BACKWARD, delta_time);
+        camera_.move(Camera::BACKWARD, delta_time);
     }
     if (engine.keys()[GLFW_KEY_A]) {
-        camera_.ProcessKeyboard(LEFT, delta_time);
+        camera_.move(Camera::LEFT, delta_time);
     }
     if (engine.keys()[GLFW_KEY_D]) {
-        camera_.ProcessKeyboard(RIGHT, delta_time);
+        camera_.move(Camera::RIGHT, delta_time);
     }
 }
 
 void MD2View::on_mouse_movement(GLfloat xoffset, GLfloat yoffset)
 {
-    camera_.ProcessMouseMovement(xoffset, yoffset);
+    camera_.process_mouse_movement(xoffset, yoffset);
 }
 
 int main(int argc, char const * argv[])
